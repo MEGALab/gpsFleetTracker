dc72720371682ccd46ad43698fc536c756c6ce66
talk2dug <talk2dug@gmail.com>
2021-11-16 17:47:32 -0600

    Added in bridge code for mqtt

--- app.js
+++ app.js
@@ -37,21 +37,22 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-////var five = require("johnny-five");
-//var board = new five.Board({
-//  port: "/dev/ttyUSB0"
-//});
+var five = require("johnny-five");
+
+var board = new five.Board({
+  port: "/dev/ttyS0"
+});
 
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
 const port = new SerialPort('/dev/ttyUSB0', {
-  baudRate: 115200
+ baudRate: 115200
 })
 
 const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
 parser.on('data', function(string){
   console.log(string)
-    //client.publish(string.toString())
+    client.publish(string)
 
 
 
@@ -62,71 +63,111 @@ var doorRelayUnlock
 var doorRelayLock
 var fuelPumpRelay
 var unKNownRelay
-/*
+var gps
 board.on("ready", function() {
+
+   gps = new five.GPS({
+    baud: 9600,
+    pins: {
+      rx: 5,
+      tx: 6,
+    }
+  });
   doorRelayUnlock = new five.Relay({
-    pin:9,
+    pin:12,
     type: "NO"
 
 
   })
   doorRelayLock = new five.Relay({
-    pin:10,
+    pin:11,
     type: "NO"
 
 
   })
 
   fuelPumpRelay = new five.Relay({
-    pin:11,
+    pin:10,
     type: "NO"
 
 
   })
   unKNownRelay = new five.Relay({
-    pin:12,
+    pin:9,
     type: "NO"
 
 
   })
-  doorRelayUnlock.close()
-  fuelPumpRelay.open()
-  doorRelayLock.close()
-  unKNownRelay.close()
+  doorRelayUnlock.open()
+  fuelPumpRelay.close()
+  doorRelayLock.open()
+  unKNownRelay.open()
+  gps.on('operations', function(msg){
+
+    console.log(msg)
+    
+    
+    })
 });
-*/
+
 var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://localhost')
 
 client.on('connect', function () {
+
+  client.publish('/fleetTracker/Jeep', 'Alpha-One Online')
   client.subscribe('/fleetTracker/Doors', function (err) {
+    if (!err) {
+      
+    }
+  })
+  client.subscribe('/fleetTracker/Pump', function (err) {
     if (!err) {
       
     }
   })
 })
 
+
+
 client.on('message', function (topic, message) {
   console.log(topic + ":" + message)
   // message is Buffer
   let command = message.toString()
-  switch (command) {
-    case 'unlock':
-      //doorRelayUnlock.open()
-      setTimeout(() => {
-        //doorRelayUnlock.close()
-        client.publish('/fleetTracker/Doors', 'unLocked')
-      }, 1000);
-      break;
-      case 'lock':
-        //doorRelayLock.open()
+  if(topic==='/fleetTracker/Pump'){
+    switch (command) {
+      case 'unlock':
+        doorRelayUnlock.close()
         setTimeout(() => {
-          //doorRelayLock.close()
-          client.publish('/fleetTracker/Doors', 'Locked')
-          
+          doorRelayUnlock.open()
+          client.publish('/fleetTracker/Doors', 'unLocked')
         }, 1000);
         break;
+      case 'lock':
+        doorRelayLock.close()
+        setTimeout(() => {
+          doorRelayLock.open()
+          client.publish('/fleetTracker/Doors', 'Locked')
+            
+        }, 1000);
+        break;
+  
+    }
   }
+  if(topic==='/fleetTracker/Pump'){
+    switch (command) {
+      case 'pumpOn':
+        fuelPumpRelay.close()
+        client.publish('/fleetTracker/Pump', 'On')
+        
+        break;
+      case 'pumpOff':
+          fuelPumpRelay.open()
+          client.publish('/fleetTracker/Pump', 'Off')
+          
+        break; 
+    }
+  }   
   console.log(message.toString())
 })
 var obj = {
@@ -138,8 +179,6 @@ var obj = {
     ALT: 231.25,
     DistFromBase:25.23,
     DirFromBase: 235
-
-
   },
   ODB:{
     Odometer: 45254.21,
@@ -153,13 +192,18 @@ var obj = {
     FuelLevel: 60,
     VehicleSpeed: 45,
     EngineSpeed: 3500,
-
-
   }
 
 
 }
 
+require('dns').resolve('www.google.com', function(err) {
+  if (err) {
+     console.log("No connection");
+  } else {
+     console.log("Connected");
+  }
+});
 
 setInterval(() => {
   client.publish('/fleetTracker/Data', JSON.stringify(obj))
